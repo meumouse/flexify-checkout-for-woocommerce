bccfd943209937913979de65e77ed821cb033a17
MeuMouse.com <95049087+meumouse@users.noreply.github.com>
2024-08-23 11:03:55 -0300

    Add files via upload

--- inc/classes/compat/class-compat-extra-fields-for-brazil.php
+++ inc/classes/compat/class-compat-extra-fields-for-brazil.php
@@ -11,6 +11,7 @@ defined( 'ABSPATH' ) || exit;
  * Compatibility with Brazilian Market on WooCommerce plugin
  *
  * @since 3.8.0
+ * @version 3.8.2
  * @package MeuMouse.com
  */
 class Extra_Fields_For_Brazil {
@@ -29,6 +30,7 @@ class Extra_Fields_For_Brazil {
      * Add compatibility with scripts on checkout
      * 
      * @since 3.8.0
+     * @version 3.8.2
      * @return void
      */
     public function compat_scripts() {
@@ -40,6 +42,22 @@ class Extra_Fields_For_Brazil {
             // Prevent conflict with jQuery mask from Brazilian Market on WooCommerce plugin
             wp_dequeue_script('jquery-mask');
             wp_deregister_script('jquery-mask');
+
+            // Dequeue the original script
+            wp_dequeue_script('woocommerce-extra-checkout-fields-for-brazil-front');
+            wp_deregister_script('woocommerce-extra-checkout-fields-for-brazil-front');
+
+            // Register a new version of the script without the 'jquery-mask' dependency
+            $suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+            wp_register_script( 'woocommerce-extra-checkout-fields-for-brazil-front-custom',
+                plugins_url() . '/woocommerce-extra-checkout-fields-for-brazil/assets/js/frontend/frontend' . $suffix . '.js',
+                array( 'jquery', 'mailcheck' ), // Removed 'jquery-mask' from dependencies
+                \Extra_Checkout_Fields_For_Brazil::VERSION,
+                true
+            );
+
+            // Enqueue the custom version of the script
+            wp_enqueue_script('woocommerce-extra-checkout-fields-for-brazil-front-custom');
         }
     }
 }
