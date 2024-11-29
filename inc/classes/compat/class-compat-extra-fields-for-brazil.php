589accdaa424c254f0e7c91751cbba639cede711
MeuMouse.com <95049087+meumouse@users.noreply.github.com>
2024-11-29 15:58:49 -0300

    Add files via upload

--- inc/classes/compat/class-compat-extra-fields-for-brazil.php
+++ inc/classes/compat/class-compat-extra-fields-for-brazil.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
  * Compatibility with Brazilian Market on WooCommerce plugin
  *
  * @since 3.8.0
- * @version 3.9.6
+ * @version 3.9.7
  * @package MeuMouse.com
  */
 class Extra_Fields_For_Brazil {
@@ -30,7 +30,7 @@ class Extra_Fields_For_Brazil {
      * Add compatibility with scripts on checkout
      * 
      * @since 3.8.0
-     * @version 3.9.6
+     * @version 3.9.7
      * @return void
      */
     public function compat_scripts() {
@@ -48,7 +48,8 @@ class Extra_Fields_For_Brazil {
             wp_deregister_script('woocommerce-extra-checkout-fields-for-brazil-front');
 
             // Register a new version of the script without the 'jquery-mask' dependency
-            $suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+            $suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
+
             wp_register_script( 'woocommerce-extra-checkout-fields-for-brazil-front-custom',
                 plugins_url() . '/woocommerce-extra-checkout-fields-for-brazil/assets/js/frontend/frontend' . $suffix . '.js',
                 array( 'jquery', 'mailcheck' ), // Removed 'jquery-mask' from dependencies
@@ -59,11 +60,19 @@ class Extra_Fields_For_Brazil {
             // Enqueue the custom version of the script
             wp_enqueue_script('woocommerce-extra-checkout-fields-for-brazil-front-custom');
             
+            $wcbcf_settings = get_option('wcbcf_settings');
+
             wp_localize_script(
                 'woocommerce-extra-checkout-fields-for-brazil-front-custom',
                 'bmwPublicParams',
                 array(
                     'maskedinput' => 'no',
+                    'state' => esc_js( __( 'State', 'woocommerce-extra-checkout-fields-for-brazil' ) ),
+                    'required' => esc_js( __( 'required', 'woocommerce-extra-checkout-fields-for-brazil' ) ),
+                    'mailcheck' => isset( $wcbcf_settings['mailcheck'] ) ? 'yes' : 'no',
+                    'only_brazil' => isset( $wcbcf_settings['only_brazil'] ) ? 'yes' : 'no',
+                    'person_type' => isset( $wcbcf_settings['person_type'] ) ? absint( $wcbcf_settings['person_type'] ) : 0,
+                    'suggest_text' => esc_js( __( 'Did you mean: %hint%?', 'woocommerce-extra-checkout-fields-for-brazil' ) ),
                 )
             );
         }
